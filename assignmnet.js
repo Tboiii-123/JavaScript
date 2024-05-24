d6a038961688c19817e28ea7cd27872344396632
Lawal Hussein <132585505+Tboiii-123@users.noreply.github.com>
2024-05-24 19:04:25 +0100

    Add files via upload

---
+++ assignmnet.js
@@ -0,0 +1,20 @@
+// var break="lawal";
+
+// // Unexpected token break
+// console.log(break);
+
+
+
+
+ let isStudent =true;
+
+ console.log(isStudent);
+
+
+
+ var a =12;
+ var b =30;
+
+ var result =a+b;
+
+ console.log(result);
